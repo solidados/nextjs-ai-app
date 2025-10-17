9843004c0eae3440aa48b52847803bd47f3d869d
solidados <geekbrains.pk@internet.ru>
2025-10-17 16:23:15 +0400

    feat: how to switch AI models; how to calc and log tokens & costs

--- src/app/api/stream/route.ts
+++ src/app/api/stream/route.ts
@@ -1,14 +1,46 @@
-import { tryCatch } from '@/utils/tryCatch';
+import { LanguageModelV2Usage } from '@ai-sdk/provider';
 import { streamText } from 'ai';
-import { openai } from '@ai-sdk/openai';
+// import { openai } from '@ai-sdk/openai';
+
+import { google } from '@ai-sdk/google'
+// import { NextResponse } from 'next/server';
 
 export const POST = async (req: Request) => {
   try {
     const { prompt } = await req.json();
   
     const result = streamText({
-      model: openai('gpt-5-nano'),
+      // model: openai('gpt-5-nano'),
+      model: google('gemini-2.0-flash-lite'),
       prompt,
+    })
+    
+    /** Logging token usage:
+     * - every `request` costs tokens
+     * - every `token` costs money*/
+    const usd = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 6,
+    })
+    
+    result.usage.then((usage: LanguageModelV2Usage) => {
+      const inputTokens = usage.inputTokens ?? 0
+      const outputTokens = usage.outputTokens ?? 0
+      const totalTokens = usage.totalTokens ?? inputTokens + outputTokens
+      
+      const INPUT_RATE= 0.10 / 1000000
+      const OUTPUT_RATE= 0.40 / 1000000
+      
+      const inputCost = inputTokens * INPUT_RATE
+      const outputCost = outputTokens * OUTPUT_RATE
+      const totalCost = inputCost + outputCost
+      
+      console.log({
+        input: { tokens: inputTokens, cost: usd.format(inputCost) },
+        output: { tokens: outputTokens, cost: usd.format(outputCost) },
+        total: { tokens: totalTokens, cost: usd.format(totalCost) }
+      })
     })
   
     return result.toUIMessageStreamResponse()
@@ -17,21 +49,4 @@ export const POST = async (req: Request) => {
     console.error("Error streaming text:", error);
     return new Response("Failed to stream text", { status: 500 });
   }
-  // const result = await tryCatch((async () => {
-  //   const { prompt } = await req.json();
-  //
-  //   const stream = streamText({
-  //     model: openai('gpt-5-nano'),
-  //     prompt,
-  //   });
-  //
-  //   return stream.toUIMessageStreamResponse();
-  // })());
-  //
-  // if (!result.ok) {
-  //   return new Response(
-  //     JSON.stringify({ error: result.error.message }),
-  //     { status: 500, headers: { 'Content-Type': 'application/json' } }
-  //   );
-  // }
 };
